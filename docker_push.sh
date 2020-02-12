97ae8904966f3b937f06db6ca341b86022120cce
Catalin Dinuta <constantin.dinuta@gmail.com>
2020-02-12 20:57:26 +0200

    building for alpine and centos

--- docker_push.sh
+++ docker_push.sh
@@ -4,8 +4,8 @@ echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
 #alpine
-docker build . -t dinutac/estuary-testrunner-alpine:"${TRAVIS_TAG}"
-docker push dinutac/estuary-testrunner-alpine:"${TRAVIS_TAG}"
+docker build . -t dinutac/estuary-testrunner:"${TRAVIS_TAG}"
+docker push dinutac/estuary-testrunner:"${TRAVIS_TAG}"
 #centos
 docker build . -t dinutac/estuary-testrunner-centos:"${TRAVIS_TAG}"
 docker push dinutac/estuary-testrunner-centos:"${TRAVIS_TAG}"
