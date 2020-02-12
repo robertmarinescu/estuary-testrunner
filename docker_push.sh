13cb43a0562d459ecde2216aaa3240743da87a24
Catalin Dinuta <constantin.dinuta@gmail.com>
2020-02-12 20:23:28 +0200

    changed name

--- docker_push.sh
+++ docker_push.sh
@@ -3,9 +3,13 @@
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKERHUB_USERNAME" --password-stdin
 
 if [[ -z "${TRAVIS_TAG}" ]]; then
-docker build . -t dinutac/estuary-viewer:"${TRAVIS_TAG}"
-docker push dinutac/estuary-viewer:"${TRAVIS_TAG}"
+docker build . -t dinutac/estuary-testrunner:"${TRAVIS_TAG}"
+docker push dinutac/estuary-testrunner:"${TRAVIS_TAG}"
 else
-docker build . -t dinutac/estuary-viewer:latest
-docker push dinutac/estuary-viewer:latest
+#alpine
+docker build . -t dinutac/estuary-testrunner:latest
+docker push dinutac/estuary-testrunner:latest
+#centos
+docker build -t dinutac/estuary-testrunner-centos:latest -f Dockerfiles/Dockerfile_centos .
+docker push dinutac/estuary-testrunner-centos:latest
 fi
